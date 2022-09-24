1e0b9a7cf4bd6edaa7b0d0e893d720b64aa26cd2
Lucas Nepomuceno <113993828+nepomucenolucass@users.noreply.github.com>
2022-09-24 17:26:20 -0300

    Add files via upload

---
+++ exercicio 1a for.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio_1a_for
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {// a) Apresentar todos os valores numéricos inteiros impares situados na
+            // faixa de 0 a 20.
+
+            for (int n1 = 0; n1 < 20;)
+            {
+                n1 = n1 + 1;
+                if (n1 % 2 != 0)
+                    Console.WriteLine(n1);
+
+            }
+        }
+    }
+}
